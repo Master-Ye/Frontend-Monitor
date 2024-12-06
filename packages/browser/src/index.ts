20cc3edb90f3005170b4289f47610d1401eaad6c
masterder <1813708165@qq.com>
2024-12-06 23:23:17 +0800

    core+plugin

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,19 +1,13 @@
-export * from './handleEvents';
-export * from './load';
-export * from './replace';
-import { setupReplace } from './load';
-import { initOptions, log } from 'frontend-monitor-core';
+export * from './plugin/handleEvents';
+export * from './plugin/load';
+export * from './plugin/replace';
+
+import { log } from 'frontend-monitor-core';
 import { _global } from 'frontend-monitor-utils';
 import { SDK_VERSION, SDK_NAME } from 'frontend-monitor-shared';
-import { InitOptions } from 'frontend-monitor-types';
-function webInit(options: InitOptions = {}): void {
-  if (!('XMLHttpRequest' in _global) || options.disabled) return;
-  initOptions(options);
-  setupReplace();
-}
+import { init } from './core/init';
 
-function init(options: InitOptions = {}): void {
-  webInit(options);
-}
+
+
 
 export { SDK_VERSION, SDK_NAME, init, log };
